5303d1d98a99815a1016bf169529685d8770bdc2
Georg Gogo. BERNHARD <gogo@bluedynamics.com>
2005-09-02 15:16:10 +0000

    Test cases added

---
+++ MoreFieldsAndWidgets/MxDateTimeField/trunk/mxdateindex/MxDateIndex.py
@@ -0,0 +1,69 @@
+# File: MxDateIndex.py
+# 
+# Copyright (c) 2005 by Bluedynamics KEG
+# Generator: ArchGenXML Version 1.4.0-beta2 devel 
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public Licence (GPL)
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+#
+__author__  = '''Georg Gogo. BERNHARD <gogo@bluedynamics.com>'''
+__docformat__ = 'plaintext'
+
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+
+from Products.PluginIndexes.DateIndex.DateIndex import DateIndex
+
+class MxDateIndex(DateIndex):
+    ''' '''
+    __implements__ = (getattr(DateIndex,'__implements__',()),)
+
+    ##code-section class-header_MxDateIndex #fill in your manual code here
+    ##/code-section class-header_MxDateIndex
+
+
+
+
+
+
+    def _convert( self, value, default=None ) :
+        """Convert Date/Time value to our internal representation"""
+        
+        if type(value) == type('') :
+            value = mx.DateTime.DateTimeFrom(value)
+        
+        if isinstance( value, mx.DateTime.mxDateTime.DateTimeType ):
+            t_tup = value.tuple()
+        else:
+            return default
+
+        yr = t_tup[0]
+        mo = t_tup[1]
+        dy = t_tup[2]
+        hr = t_tup[3]
+        mn = t_tup[4]
+
+        t_val = ( ( ( ( yr * 12 + mo ) * 31 + dy ) * 24 + hr ) * 60 + mn )
+
+        return t_val
+
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
