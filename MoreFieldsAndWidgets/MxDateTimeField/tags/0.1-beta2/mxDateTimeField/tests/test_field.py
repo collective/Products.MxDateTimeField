e22936d9bdc61b1987ec1fe7f721cb01e38b06fb
Harald Friessnegger <frisi@users.noreply.github.com>
2006-01-24 10:15:51 +0000

    created a tag to have a defined state for productive deployment

---
+++ MoreFieldsAndWidgets/MxDateTimeField/tags/0.1-beta2/mxDateTimeField/tests/test_field.py
@@ -0,0 +1,70 @@
+# File: test_field.py
+# 
+# Copyright (c) 2005 by Bluedynamics KEG
+# Generator: ArchGenXML Version 1.4.0-beta2 devel 
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public Licence (GPL)
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+#
+__author__  = '''Georg Gogo. BERNHARD <gogo@bluedynamics.com>'''
+__docformat__ = 'plaintext'
+
+##code-section module-header #fill in your manual code here
+##/code-section module-header
+
+#
+# test-cases for class(es) MxDateTimeType
+#
+import os, sys
+from Testing import ZopeTestCase
+from Products.MxDateTimeField.tests.MxDateTestCase import MxDateTestCase
+# import the tested classes
+from Products.MxDateTimeField.testclasses.MxDateTimeType import MxDateTimeType
+
+class test_field(MxDateTestCase):
+    """ test-cases for class(es) 
+    """
+
+    ##code-section class-header_test_field #fill in your manual code here
+    ##/code-section class-header_test_field
+
+    def afterSetUp(self):
+        """
+        """
+        pass
+
+
+    # Manually created methods
+
+
+def test_suite():
+    from unittest import TestSuite
+    from Testing.ZopeTestCase.zopedoctest import ZopeDocFileSuite
+
+    return TestSuite((
+        ZopeDocFileSuite('test_field.txt',
+                         package='Products.MxDateTimeField.doc',
+                         test_class=test_field),
+    ))
+
+
+if __name__ == '__main__':
+    execfile(os.path.join(sys.path[0], 'framework.py'))
+
+##code-section module-footer #fill in your manual code here
+##/code-section module-footer
+
+
+
